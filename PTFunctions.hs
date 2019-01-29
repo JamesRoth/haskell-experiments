7dd03a9b2559409168618990297a73c7baacca5e
jaRometh27 <jarometh27@hanovernorwichschools.org>
2019-01-29 15:07:25 +0000

    Homework #1 - Functions

--- PTFunctions.hs
+++ PTFunctions.hs
@@ -1,1 +1,14 @@
 module PTFunctions where
+
+quadraticVertex a b c = (-b)/(2*a)
+
+quadraticDiscriminant a b c = (b*b) - (4*a*c)
+
+quadraticRealSolutionQty a b c = (if quadraticDiscriminant a b c < 0 then 0 else if quadraticDiscriminant a b c == 0 then 1 else 2)
+
+convertFtoC f = (f-32)/1.8
+
+convertCtoF c = (1.8*c)+32
+
+convertTemp t unit = if unit == "c" then (1.8*t)+32 else (t-32)/1.8
+
